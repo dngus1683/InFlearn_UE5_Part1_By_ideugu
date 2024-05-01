daba4611e372c33601dbfbaa441f4e057c5bdadf
Jangwh <dngus1683@naver.com>
2024-05-01 22:59:32 +0900

    7강 실습 추가

---
+++ Source/inflearn_Part_1/StaffInterface1.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PersonInterface1.h"
+#include "StaffInterface1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFLEARN_PART_1_API UStaffInterface1 : public UPersonInterface1
+{
+	GENERATED_BODY()
+
+public:
+	UStaffInterface1();
+	
+};
