3df3a9f70e2fcb4c52e742a618286ad998574ab5
Jangwh <dngus1683@naver.com>
2024-09-15 22:06:56 +0900

    13장 실습 추가

--- Source/inflearn_Part_1/inflearn_Part_1.Build.cs
+++ Source/inflearn_Part_1/inflearn_Part_1.Build.cs
@@ -8,7 +8,7 @@ public class inflearn_Part_1 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
